ea409fb1c7989fe7fcc9614f343d43ac414b4b72
Andrey Popp <8mayday@gmail.com>
2019-10-25 18:41:24 +0300

    mode.buffer_watcher: track columns (opt in)

--- lua/mode/buffer_watcher.lua
+++ lua/mode/buffer_watcher.lua
@@ -1,3 +1,7 @@
+-- BufferWatcher
+--
+-- A mechanism to be notified of when a specified buffer has changed.
+
 local async = require 'mode.async'
 local util = require 'mode.util'
 local vim = require 'mode.vim'
@@ -9,6 +13,12 @@ function BufferWatcher:init(o)
   self.buffer = o.buffer
   self.is_utf8 = o.is_utf8 == nil and true or o.is_utf8
   self.is_shutdown = false
+  self.track_start_column = o.track_start_column or false
+  if self.track_start_column then
+    self.lines = self.buffer:contents_lines()
+  else
+    self.lines = nil
+  end
   self:_start()
 end
 
@@ -18,10 +28,34 @@ function BufferWatcher:_start()
       if self.is_shutdown then
         return true
       end
+
+      local start_coln = nil
+      if self.track_start_column then
+        local line_1 = self.lines[start + 1]
+        local updated_lines = self.buffer:contents_lines(start, stop)
+        local updated_line_1 = updated_lines[1]
+
+        start_coln = 1
+        for ch in line_1:gmatch(".") do
+          local updated_ch = string.char(updated_line_1:byte(start_coln))
+          if ch ~= updated_ch then
+            start_coln = start_coln - 1 -- make it zero based
+            break
+          end
+          start_coln = start_coln + 1
+        end
+
+        -- Update self.lines with updated lines
+        for lnum,line in ipairs(updated_lines) do
+          self.lines[start+lnum] = line
+        end
+      end
+
       self.updates:put {
         buffer = self.buffer,
         tick = tick,
         start = start,
+        start_coln = start_coln,
         stop = stop,
         stopped = stopped,
         bytes = bytes,
