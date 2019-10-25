a03c4cf8fd6f3f7b584e9acab4f868f730c11a89
Andrey Popp <8mayday@gmail.com>
2019-10-25 20:51:24 +0300

    mode.buffer_watcher: fixes to start_column tracking

--- lua/mode/buffer_watcher.lua
+++ lua/mode/buffer_watcher.lua
@@ -35,20 +35,43 @@ function BufferWatcher:_start()
         local updated_lines = self.buffer:contents_lines(start, stopped)
         local updated_line_1 = updated_lines[1]
 
-        start_coln = 1
-        for ch in line_1:gmatch(".") do
-          local updated_ch = string.char(updated_line_1:byte(start_coln))
-          if ch ~= updated_ch then
-            start_coln = start_coln - 1 -- make it zero based
-            break
+        -- Compare first line of the change
+        start_coln = 0
+        if updated_line_1 ~= nil and line_1 ~= nil then
+          for ch in line_1:gmatch(".") do
+            local updated_ch = string.char(updated_line_1:byte(start_coln + 1))
+            if ch ~= updated_ch then
+              start_coln = start_coln
+              break
+            end
+            start_coln = start_coln + 1
           end
-          start_coln = start_coln + 1
         end
 
         -- Update self.lines with updated lines
-        for lnum,line in ipairs(updated_lines) do
-          self.lines[start+lnum] = line
+        local new_lines = {}
+        -- Before
+        local lnum = 1
+        while lnum < start + 1 do
+          table.insert(new_lines, self.lines[lnum])
+          lnum = lnum + 1
         end
+        -- updates
+        for _,line in ipairs(updated_lines) do
+          table.insert(new_lines, line)
+        end
+        -- After
+        lnum = stop + 1
+        while true do
+          local line = self.lines[lnum]
+          if line == nil then
+            break
+          else
+            table.insert(new_lines, line)
+            lnum = lnum + 1
+          end
+        end
+        self.lines = new_lines
       end
 
       self.updates:put {
