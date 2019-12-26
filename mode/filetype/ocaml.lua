fad7de38837275413d68e0520ddf14493ec6d68c
Andrey Popp <8mayday@gmail.com>
2019-12-26 09:54:54 +0300

    ocaml: ocaml-lsp-server

--- mode/filetype/ocaml.lua
+++ mode/filetype/ocaml.lua
@@ -1,23 +1,33 @@
 local mode = require 'mode'
 local fs = require 'mode.fs'
 
+local function is_esy(p)
+  return fs.exists(p / 'esy.json') or fs.exists(p / 'package.json')
+end
+
+local exe = 'ocaml-lsp-server'
+
 mode.configure_lsp {
-  id = 'merlin',
+  id = 'ocaml-lsp',
   languageId = 'ocaml',
   filetype = {'ocaml', 'reason'},
-  command = function(_)
-    local cmd = 'esy'
-    local args = {
-      'exec-command',
-      '--include-build-env',
-      '--include-current-env',
-      'ocamlmerlin-lsp'
-    }
-    return cmd, args
+  command = function(root)
+    if is_esy(root) then
+      local cmd = 'esy'
+      local args = {
+        'exec-command',
+        '--include-build-env',
+        '--include-current-env',
+        exe
+      }
+      return cmd, args
+    else
+      return exe, {}
+    end
   end,
   root = function(filename)
     return fs.find_closest_ancestor(filename.parent, function(p)
-      return fs.exists(p / 'esy.json') or fs.exists(p / 'package.json')
+      return is_esy(p) or fs.exists(p / 'dune-project')
     end)
   end
 }
